bad75eafa9f20cbd9f1d8eb1de52477e8757e882
azenovich <alexander.zenovich@gmail.com>
2018-03-12 19:01:57 +0300

    Header was added. Depenedencies was included fontawesome-scss and nornalize.

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ const browserSync = require('browser-sync').create();
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
 const spritesmith = require('gulp.spritesmith');
+const merge = require('merge-stream');
 const rimraf = require('rimraf');
 const rename = require('gulp-rename');
-const merge = require('merge-stream');
 
 
-/*----------  Server  ----------*/
+/*----------  Local server  ----------*/
 
 gulp.task('server', function() {
     browserSync.init({
